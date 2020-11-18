0ff7d7fca179bdb32918f9d6f59b9a7e4487b645
DorcasWanjiku <smallwanjiku@gmail.com>
2020-11-18 12:13:56 +0300

    deploy to heroku

--- virtual/bin/django-admin.py
+++ virtual/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/moringa/Documents/image-gallery/virtual/bin/python3.6
+#!/home/moringa/Desktop/image-gallery/virtual/bin/python3.6
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
